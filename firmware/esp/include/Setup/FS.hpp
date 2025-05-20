7131aa238291f24d240d446ecea6caff2a9cdf52
Oskar <ich@astrobeschiss.de>
2025-05-20 09:36:59 +0200

    Fix missing ifdef print in FS.hpp

--- firmware/esp/include/Setup/FS.hpp
+++ firmware/esp/include/Setup/FS.hpp
@@ -6,11 +6,14 @@
 
 void setupFS()
 {
+#ifdef SETUP_SERIAL
     Serial.println("Setting up LittleFS");
-
+#endif
     if (!LittleFS.begin(true))
     {
+#ifdef SETUP_SERIAL
         Serial.println("Could not mount LittleFS; Flash reformatted");
+#endif
     }
 };
 
