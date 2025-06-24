515527f3515da480f0c8fde1cb469f7b8e617495
MrGr4ch <104979051+MrGr4ch@users.noreply.github.com>
2025-06-24 11:39:17 +0200

    working parallel reading and writing

--- firmware/esp/src/main.cpp
+++ firmware/esp/src/main.cpp
@@ -8,7 +8,67 @@
 #include "Setup/Speaker.hpp"
 
 #include <lwip/sockets.h>
-int audio_sock;
+static int audio_sock;
+bool writing;
+bool reading;
+TaskHandle_t write_handle = NULL;
+TaskHandle_t read_handle = NULL;
+
+void write_task(void*client_socketp){
+    Serial.printf("started write task \n");
+
+    Serial.flush();
+    size_t bytes_left = 0;
+    size_t bytes_written = 0;
+
+    int16_t speaker_buf[64];
+    speaker_buf[0] = 18;
+
+    size_t offset = 0;
+
+    int client_socket = *((int*)(client_socketp));
+
+    while(writing){
+        bytes_left = read(client_socket, speaker_buf, sizeof(speaker_buf));
+
+        while (bytes_left % 2 != 0)
+            bytes_left += read(client_socket, ((int8_t *)speaker_buf) + bytes_left, 1);
+        offset = 0;
+        while (bytes_left > 0)
+        {   
+            i2s_write(SPEAKER_I2S_NUM, ((int8_t*)speaker_buf) + offset, bytes_left, &bytes_written, portMAX_DELAY);
+            bytes_left -= bytes_written;
+            offset += bytes_written;
+        }
+    }
+}
+
+void read_task(void*client_socketf){
+    Serial.printf("started read task \n");
+
+    size_t bytes_left = 0;
+    size_t bytes_written = 0;
+
+    int16_t buf[64];
+    buf[0] = 17;
+
+    size_t offset = 0;
+
+    int client_socket = *((int*)(client_socketf));
+
+    while(reading){
+        i2s_read(MIC_I2S_NUM, &buf, sizeof(buf), &bytes_left, portMAX_DELAY);
+
+        size_t offset = 0;
+        while (bytes_left > 0)
+        {   
+            bytes_written = write(client_socket, ((int8_t*)buf) + offset, bytes_left);
+            bytes_left -= bytes_written;
+            offset += bytes_written;
+        }
+    }
+}
+
 void setup()
 {
 #ifdef SETUP_SERIAL
@@ -65,33 +125,56 @@ void loop()
 
     size_t offset = 0;
 
+    reading = 1;
+    writing = 1;
+
+    xTaskCreate(
+        write_task,
+        "writing task",
+        10000,
+        (void*)(&client_socket),
+        1,
+        &write_handle
+    );
+
+    xTaskCreate(
+        read_task,
+        "read_task",
+        10000,
+        &client_socket,
+        2,
+        &read_handle
+    );
+
     while (1)
     {
-        //Serial.println("READING MIC");
-        //bytes_left = readMic((int16_t*) buf ,sizeof(buf));
-        i2s_read(MIC_I2S_NUM, &buf, sizeof(buf), &bytes_left, portMAX_DELAY);
+        // //Serial.println("READING MIC");
+        // //bytes_left = readMic((int16_t*) buf ,sizeof(buf));
+        // i2s_read(MIC_I2S_NUM, &buf, sizeof(buf), &bytes_left, portMAX_DELAY);
 
-        // Serial.printf("READ %d BYTES ",bytes_left);
-        // Serial.printf("Buffer first Byte: %d \n", buf[0]);
-        size_t offset = 0;
-        while (bytes_left > 0)
-        {   
-            bytes_written = write(client_socket, ((int8_t*)buf) + offset, bytes_left);
-            bytes_left -= bytes_written;
-            offset += bytes_written;
-        }
+        // // Serial.printf("READ %d BYTES ",bytes_left);
+        // // Serial.printf("Buffer first Byte: %d \n", buf[0]);
+        // size_t offset = 0;
+        // while (bytes_left > 0)
+        // {   
+        //     bytes_written = write(client_socket, ((int8_t*)buf) + offset, bytes_left);
+        //     bytes_left -= bytes_written;
+        //     offset += bytes_written;
+        // }
 
-        bytes_left = read(client_socket, speaker_buf, sizeof(speaker_buf));
+        // bytes_left = read(client_socket, speaker_buf, sizeof(speaker_buf));
 
-        while (bytes_left % 2 != 0)
-            bytes_left += read(client_socket, ((int8_t *)speaker_buf) + bytes_left, 1);
-        Serial.printf("outside loop \n");
-        offset = 0;
-        while (bytes_left > 0)
-        {   
-            i2s_write(SPEAKER_I2S_NUM, ((int8_t*)speaker_buf) + offset, bytes_left, &bytes_written, portMAX_DELAY);
-            bytes_left -= bytes_written;
-            offset += bytes_written;
-        }
+        // while (bytes_left % 2 != 0)
+        //     bytes_left += read(client_socket, ((int8_t *)speaker_buf) + bytes_left, 1);
+        // offset = 0;
+        // while (bytes_left > 0)
+        // {   
+        //     i2s_write(SPEAKER_I2S_NUM, ((int8_t*)speaker_buf) + offset, bytes_left, &bytes_written, portMAX_DELAY);
+        //     bytes_left -= bytes_written;
+        //     offset += bytes_written;
+        // }
+        sleep(1);
+        Serial.printf("running \n");
+
     }
 }
