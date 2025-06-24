ef09da11257ccb3cc5dc0454aedc2aa488bb3c5b
Robert Karl Horn <robert.karl.horn@informatik.hu-berlin.de>
2025-06-24 12:00:45 +0200

    redundanter code entfernt

--- firmware/esp/src/main.cpp
+++ firmware/esp/src/main.cpp
@@ -115,16 +115,6 @@ void loop()
     }
     printf("connected\n");
 
-    size_t bytes_left = 0;
-    size_t bytes_written = 0;
-    int16_t buf[64];
-    buf[0] = 17;
-
-    int16_t speaker_buf[64];
-    speaker_buf[0] = 18;
-
-    size_t offset = 0;
-
     reading = 1;
     writing = 1;
 
@@ -148,33 +138,8 @@ void loop()
 
     while (1)
     {
-        // //Serial.println("READING MIC");
-        // //bytes_left = readMic((int16_t*) buf ,sizeof(buf));
-        // i2s_read(MIC_I2S_NUM, &buf, sizeof(buf), &bytes_left, portMAX_DELAY);
-
-        // // Serial.printf("READ %d BYTES ",bytes_left);
-        // // Serial.printf("Buffer first Byte: %d \n", buf[0]);
-        // size_t offset = 0;
-        // while (bytes_left > 0)
-        // {   
-        //     bytes_written = write(client_socket, ((int8_t*)buf) + offset, bytes_left);
-        //     bytes_left -= bytes_written;
-        //     offset += bytes_written;
-        // }
-
-        // bytes_left = read(client_socket, speaker_buf, sizeof(speaker_buf));
-
-        // while (bytes_left % 2 != 0)
-        //     bytes_left += read(client_socket, ((int8_t *)speaker_buf) + bytes_left, 1);
-        // offset = 0;
-        // while (bytes_left > 0)
-        // {   
-        //     i2s_write(SPEAKER_I2S_NUM, ((int8_t*)speaker_buf) + offset, bytes_left, &bytes_written, portMAX_DELAY);
-        //     bytes_left -= bytes_written;
-        //     offset += bytes_written;
-        // }
         sleep(1);
-        Serial.printf("running \n");
+        //todo: do stuff
 
     }
 }
