06d8799d5e38aac498d267c08128c8446204a439
MrGr4ch <lpgrach@gmail.com>
2025-07-02 21:23:44 +0200

    General build of mixing class created

--- services/connector/src/mixing_class.py
+++ services/connector/src/mixing_class.py
@@ -1,4 +1,6 @@
 import asyncio
+from dis import Instruction
+from time import sleep
 
 from mixmode import *
 from streamnode import Node
@@ -33,6 +35,21 @@ class mixing_mode(Node):
             if isinstance(schritt, InstructionStep):
                 self.data_send(schritt.beschreibung.encode())
 
+            elif isinstance(schritt, IngredientStep):
+                self.data_send(schritt.beschreibung.encode())
 
+                #Gewicht an Wage zur überprüfung senden
+                #handle_weight_data(schritt.menge)
+
+            while self.queue.empty():
+                sleep(5)
+
+            answer = self.queue.get_nowait()
+            if answer == 1:
+                summary_of_mixing_process += schritt.beschreibung + " pause "
+                continue
+            else:
+                summary_of_mixing_process += schritt.beschreibung + " pause " + "Process cancelled"
+                continue
 
         return summary_of_mixing_process
